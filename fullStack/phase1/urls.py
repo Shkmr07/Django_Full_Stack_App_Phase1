a5bd6d371d06c2ab17910b88705a6716894658cf
Shkmr07 <shkmrsingh@gmail.com>
2024-08-15 17:16:51 +0530

    Only Frontend files

--- fullStack/phase1/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import TodoView,TodoEditDeleteView,TodoCreateView
-
-urlpatterns = [
-    path('',TodoView.as_view(),name='todo'),
-    path('edit/<int:pk>/',TodoEditDeleteView.as_view(),name='edit'),
-    path('create/',TodoCreateView.as_view(),name='create')
-]
